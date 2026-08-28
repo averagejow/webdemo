f6a0a9031be8bbc27152864b89ef898e18a232ec
averagejow <joshuajardin.tan@gmail.com>
2026-08-28 20:33:51 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import Home from './components/Home'
-import Section from './components/Section'
-import { Route, Routes } from 'react-router'
+
 import './App.css'
 
 function App() {
@@ -8,12 +6,7 @@ function App() {
   return (
     <div className='flex flex-col w-100 h-100'>     
         <a href='#/home'>Home</a>
-
-        <Routes>  
-          <Route index element={<Home />}/>
-          <Route path="/home" element={<Home />}/>
-          <Route path='/section' element={<Section />}/>
-        </Routes>
+        
     </div>          
   )
 }
